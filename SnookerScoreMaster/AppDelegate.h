8e571fc6ad14fb0216d356e21364d3673e9eb514
Andrew Glew <drewglew@gmail.com>
2014-11-21 21:18:11 +0100

    Initial Commit

---
+++ SnookerScoreMaster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SnookerScoreMaster
+//
+//  Created by andrew glew on 21/11/2014.
+//  Copyright (c) 2014 andrew glew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
